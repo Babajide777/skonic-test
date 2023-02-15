cdddfd97e054887166708174934593085bee1f6e
Babajide Peter Oyafemi <67855455+Babajide777@users.noreply.github.com>
2023-02-15 08:23:33 -0600

    added popup component

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "@next/font/google";
+import PopUp from "@/components/PopUp";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +13,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Jide</main>
+      <main>
+        <PopUp />
+        <button className="main-btn">Click Here</button>
+      </main>
     </>
   );
 }
